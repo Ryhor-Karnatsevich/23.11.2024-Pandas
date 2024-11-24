0d83429591219ef9ca67bcf7047eebafa55307b0
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2024-11-24 21:10:02 +0000

    1

--- pandass.py
+++ pandass.py
@@ -2,6 +2,9 @@ import pandas as pd
 
 # Загрузка CSV файла
 table = pd.read_csv("11.csv",index_col = 0)
+for ele,ele2 in table.iterrows():
+    table.loc[ele,'Big City'] = ele2['city'].upper()
+#table["length"] = table["name"].apply(len)
 print(table)
 
 
