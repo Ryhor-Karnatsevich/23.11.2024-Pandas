f1c3b2a1d136a7058b8956957b272f519763d042
Ryhor-Karnatsevich <spamjamesmorgan@gmail.com>
2024-11-25 14:58:18 +0000

    q

--- pandass.py
+++ pandass.py
@@ -5,8 +5,18 @@ table = pd.read_csv("11.csv",index_col = 0)
 for ele,ele2 in table.iterrows():
     table.loc[ele,'Big City'] = ele2['city'].upper()
 #table["length"] = table["name"].apply(len)
-print(table)
 
+print(table)
+#print(table.head())
+#print(table.info())
+#print(table.shape)
+#print(table.describe())
+
+#print(table.values)
+#print(table.index)
+#print(table.columns)
+print(table.sort_values(['age','experience'],ascending=[True,False])) 
+print(table[['age','experience']])
 
 
 
